5351aa73771e08397d982aa66a5d667d73f6eb15
codetaha <taha.kachwala21@gmail.com>
2016-08-15 00:11:18 +0300

    some test code

---
+++ src/components/chat_module/chatList.js
@@ -0,0 +1,31 @@
+"use strict";
+var React = require('react');
+
+
+var GroupBar = require('./groupBar.js');
+
+var ChatList = React.createClass({
+	propTypes: {
+		// Checks if prop is passed by the Parent component
+		groups: React.PropTypes.array.isRequired,
+		onClick: React.PropTypes.func.isRequired,
+		selectedGroupId: React.PropTypes.string
+	},
+	render: function() {
+		return (
+			<div>
+				<ul className="list-group">
+				<li className="list-group-item">Search option
+				</li>
+				<GroupBar 
+					groups={this.props.groups}
+					onClick={this.props.onClick}
+					selectedGroupId={this.props.selectedGroupId}
+				/>
+				</ul>
+			</div>
+		);
+	}
+});
+
+module.exports = ChatList;
