cd9b25b91a18ddcabeb013899392a3031b25d18a
codetaha <taha.kachwala21@gmail.com>
2016-08-14 18:14:33 +0300

    Attaching and sending images completed

---
+++ src/components/chat_module/chatInput.jsx
@@ -0,0 +1,67 @@
+"use strict";
+var React = require('react');
+
+var ChatInput = React.createClass({
+	propTypes: {
+		// Checks if prop is passed by the Parent component
+		onChange: React.PropTypes.func.isRequired,
+		message: React.PropTypes.object.isRequired,
+		onSend: React.PropTypes.func.isRequired,
+	},
+	render: function() {
+		//console.log(this.props.message)
+		var attached="";
+		if(this.props.message.media){
+			attached = "File Attached: "+this.props.message.mediaParams.file.name;
+
+		}
+		return (
+			<form>
+				<div className="input-group">
+					<input id="btn-input" type="text" className="form-control input-sm" placeholder="Type your message here..."
+						name='text'
+						ref='message'
+						onChange={this.props.onChange}
+						value={this.props.message.text}
+					/>
+
+					{/*<input 
+						className="form-control"
+						type='file'
+						accept=".png,.gif,.jpg,.jpeg"
+						name='mediaPath'
+						onChange={this.props.onChange}
+						value=''
+					/>*/}
+					<span className="input-group-btn">
+						<span className="btn btn-default btn-sm btn-file">
+							<span className="glyphicon glyphicon-paperclip" aria-hidden="true">
+							</span>
+							<input type="file" 
+								className="form-control"
+								type='file'
+								accept=".png,.gif,.jpg,.jpeg"
+								name='mediaPath'
+								onChange={this.props.onChange}
+							/>
+						</span>
+						<input 
+							type="submit"
+							value="Send"
+							onClick={this.props.onSend}
+							className="btn btn-warning btn-sm" id="btn-chat"
+						/>
+					</span>
+				</div>
+				<label>
+					{attached}
+				</label>
+			</form>
+		);
+	}
+});
+/*
+						onChange={this.props.onChange}
+						value={this.props.message.mediaParams.path}
+*/
+module.exports = ChatInput;
